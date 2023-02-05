bc4dda123a922755f1f6663543834c38b2c1ba79
Cocoandjane <janeliangbc@gmail.com>
2023-02-05 11:25:04 -0800

    d

--- components/index.js
+++ components/index.js
@@ -4,10 +4,10 @@ export {default as CTA} from './header/CTA'
 export {default as Socials} from './header/Socials'
 export {default as Header} from './header/Header'
 export {default as About} from './about/About'
-export {default as Contact} from './contact/Contact'
-export {default as Services} from './services/Services'
+export {default as Contact} from './Contact/Contact'
+export {default as Services} from './Services/Services'
 export {default as Projects} from './projects/Projects'
-export {default as Footer} from './footer/Footer'
+export {default as Footer} from './Footer/Footer'
 export {default as SingleProject} from './projects/Project'
 
 
