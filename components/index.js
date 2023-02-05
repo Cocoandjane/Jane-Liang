3c5761ee7ddd08c773aaf9fd516f15964b763c88
Cocoandjane <janeliangbc@gmail.com>
2023-02-05 11:18:24 -0800

    c

--- components/index.js
+++ components/index.js
@@ -7,7 +7,7 @@ export {default as About} from './about/About'
 export {default as Contact} from './contact/Contact'
 export {default as Services} from './services/Services'
 export {default as Projects} from './projects/Projects'
-export {default as Footer} from './Footer/Footer'
-export {default as SingleProject} from './Projects/Project'
+export {default as Footer} from './footer/Footer'
+export {default as SingleProject} from './projects/Project'
 
 
