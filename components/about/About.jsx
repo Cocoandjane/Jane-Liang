2936b293e1df2d70a4913c429fe82f29a5e489dd
Cocoandjane <janeliangbc@gmail.com>
2023-02-05 21:32:27 -0800

    g

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -3,15 +3,9 @@ export default function About() {
   return (
     <section className="about-me" id="about">
       <h2 >About me</h2>
-      <p>
-        I remember the first time I stumbled upon the world of web development, I was immediately captivated by the endless possibilities it offered. I knew that this was the path I wanted to take. I embarked on a journey to master the art and science of building websites and web applications.
-      </p>
-      <br/>
-      <p>After countless hours of studying and practice, I am proud to say that I have graduated as a full stack web developer, equipped with a formidable set of technical skills including HTML/CSS, JavaScript, Node.js, Express.js, React, Next.js, C#, PHP, ASP.NET, MySQL, PostgreSQL, MongoDB, Firebase and more.</p>
-      <br/>
-      <p>However, my journey didn't stop there. I also had the opportunity to gain previous experience in other fields such as office administration and retail. This allowed me to develop a diverse perspective and adaptability which I believe will be an asset in the tech industry.</p>
-      <br/>
-      <p>Now, I am eager to take on new challenges and apply my skills and experiences to make a meaningful impact in the tech industry. I am excited to see where this journey will take me next.</p>
+      <p>With a passion for technology and a drive to create, I ventured into the world of web development. I was inspired by its limitless potential and knew that this was my calling. Through hard work and dedication, I honed my skills and became a full-stack web developer, proficient in HTML/CSS, JavaScript, Node.js, React, C#, PHP, ASP.NET, and a variety of databases.
+        
+      My background in logistics, office administration, and retail, along with my certification in Microsoft Office, provide a well-rounded perspective that will allow me to approach web development projects with creative problem-solving and real-world practicality. I am eager to take on new challenges and make a meaningful impact in this dynamic and ever-evolving field.</p>
     </section>
   )
 }
