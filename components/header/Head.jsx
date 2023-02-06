344d5fb37fddd9028aea5b35cc710cf456a97da5
Cocoandjane <janeliangbc@gmail.com>
2023-02-05 20:08:47 -0800

    g

--- components/header/Head.jsx
+++ components/header/Head.jsx
@@ -6,7 +6,7 @@ function IndexPage() {
       <Head>
       <title>Jane Liang - Full-stack Web Developer</title>
         <meta property="og:title" content="My page title" key="title" />
-        <link rel="icon" type="image/x-ion" href="/favicon1.svg" className='rounded-lg'/>
+        <link rel="icon" type="image/x-ion" href="/logo2.svg" className='rounded-lg'/>
       </Head>
     </div>
   )
