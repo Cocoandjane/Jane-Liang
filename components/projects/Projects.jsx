0ca0eee3d24428d7721c7d5be24a096e0333b568
Cocoandjane <janeliangbc@gmail.com>
2023-02-08 13:22:12 -0800

    abc

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -5,21 +5,23 @@ import { TbListDetails } from 'react-icons/tb'
 import Image from 'next/image'
 export default function Projects({ projects }) {
 
+
   return (
     <section id="projects" >
       <h5>My Recent Work</h5>
       <h2>Projects</h2>
       <div className="container portfolio__container">
         {
-          projects.map((project) => {
+          projects.sort((a, b) => b.id - a.id).map((project) => {
             return (<article className="portfolio__item" key={project.id}>
               <div className="portfolio__item-image">
+                <a href={`/projects/${project.id}`}>
                 <Image
                   width={500}
                   height={500}
                   alt=""
                   className='project-image'
-                  src={project.image} />
+                  src={project.image} /></a>
                 <h3>{project.title}</h3>
                 <div className="portfolio__item-cta">
                   <a href={project.github}  target="_blank" rel="noopener noreferrer" ><AiOutlineGithub /></a>
